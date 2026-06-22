42ead47b9a84cae6f3b4e145380af5ff781ded32
RenzPaoloAyuban <ayubanrenzpaolo@gmail.com>
2026-06-22 09:53:22 +0800

    renamed metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "(WIP) Renz Paolo Ayuban | Portfolio",
+  title: "Renz Paolo Ayuban | Portfolio",
   description: "Portfolio",
 };
 
